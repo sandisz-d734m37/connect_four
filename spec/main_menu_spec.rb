e6e38122b62825011ff389bf00c77139d8302d17
John Hennerich <john.hennerich@gmail.com>
2022-02-07 21:22:44 -0700

    Inital commit

---
+++ spec/main_menu_spec.rb
@@ -0,0 +1,24 @@
+#welcome_msg
+#displays banner to user
+#Welcome to CONNECT FOUR
+#Enter p to play. Enter q to quit.
+
+#user_interaction
+#from welcom_msg reads p or q. otherwise exit program
+#request user to place a piece
+#Please enter A-G to drop your game piece
+#expect A-G from user
+#during a current game read A-G to place piece
+
+#start_game
+#if p start the game, if q exit
+
+#display empty_game_board
+# 6 by 7 table with header of A-G
+#ABCDEFG
+#.......
+#.......
+#.......
+#.......
+#.......
+#.......
