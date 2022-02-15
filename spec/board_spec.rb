a47de3f62ad6c968a683ac21ae9e5835df576579
phdd0 <paul.haddick@outlook.com>
2022-02-15 14:55:40 -0700

    test: board test spec complete

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,10 +8,11 @@ RSpec.describe Board do
     expect(board).to be_an_instance_of(Board)
   end
 
-  xit "can display the board" do
-    board = Board.new
-    expect(board.display).to output.to_board
-  end
+  it "can display the board" do
+      board = Board.new
+      printed = "ABCDEFG\n.......\n.......\n.......\n.......\n.......\n.......\n"
+      expect{ board.display }.to output(printed).to_stdout
+    end
 
   it "can place a piece" do
     board = Board.new
@@ -58,6 +59,42 @@ RSpec.describe Board do
     expect(board.board.transpose[4]).not_to include('X')
   end
 
+  it "Does not find win at 3 up diagonal same entries; does on 4th" do
+    board = Board.new
+    board.place_piece('C','O')
+    board.place_piece('D','█')
+    board.place_piece('E','█')
+    board.place_piece('D','O')
+    board.place_piece('E','█')
+    board.place_piece('E','O')
+    board.place_piece('F','█')
+    board.place_piece('F','█')
+    board.place_piece('F','O')
+    expect(board.check_upward_diagonals).to be false
+    expect(board.won?).to be false
+    board.place_piece('F','O')
+    expect(board.check_upward_diagonals).to be true
+    expect(board.won?).to be true
+  end
+
+  it "Does not find win at 3 down diagonal same entries; does on 4th" do
+    board = Board.new
+    board.place_piece('C','█')
+    board.place_piece('C','█')
+    board.place_piece('C','█')
+    board.place_piece('C','O')
+    board.place_piece('D','█')
+    board.place_piece('D','█')
+    board.place_piece('D','O')
+    board.place_piece('E','█')
+    board.place_piece('E','O')
+    expect(board.check_downward_diagonals).to be false
+    expect(board.won?).to be false
+    board.place_piece('F','O')
+    expect(board.check_downward_diagonals).to be true
+    expect(board.won?).to be true
+  end
+
   it "Can determine if the board is full" do
     board = Board.new
 
@@ -70,4 +107,22 @@ RSpec.describe Board do
     expect(board.board_is_full?).to be true
   end
 
+  it "can return won condition via .won? method" do
+    board = Board.new
+
+    expect(board.won?).to be false
+
+    until board.check_rows == true
+      board.place_piece('G', 'X')
+      end
+    expect(board.won?).to be true
+      end
+
+    it "can reset board to empty state" do
+        board = Board.new
+        board.place_piece('A','X')
+        expect(board.board[5][0]).to eq('X')
+        board.clear_board
+        expect(board.board[5][0]).to eq('.')
+      end
 end
