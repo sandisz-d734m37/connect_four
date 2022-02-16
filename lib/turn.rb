795d7f7d5d3fd221a7e5b82a1297995092df086b
John Hennerich <john.hennerich@gmail.com>
2022-02-16 14:30:37 -0700

    Refactor: clean and comment project add README content

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,13 @@
 require './lib/user_interface'
 require './lib/board'
+###----------------------------------------
+# Turn: Game logic for taking turn between the
+#       player and computer
+###----------------------------------------
 class Turn
   attr_reader :board
 
   def initialize(board)
-#  def initialize
-#   @turn_type = turn_type
     @user_interface = UserInterface.new
     @board = board
   end
@@ -14,34 +16,26 @@ class Turn
     @user_interface.place_msg
   end
 
- ###########################################################################
-  #def playagain                               # deprecate until re-factor
-  #  puts "Would you like to play again?"
-  #  answer = @user_interface.getinput
-  #  if answer.upcase == "Y"
-  #    return true
-  #  end
-  #end
- ###########################################################################
-
   def take_turn(type)
-#moved to end of until loop in runner    @board.display
+
     if type == "human"
 
       user_response = @user_interface.getinput.upcase
       until user_response.match(/[A-G]/) && user_response.length == 1
+      # loop with verifyed input of a single letter A-G
+
         @user_interface.place_msg
         user_response = @user_interface.getinput.upcase
       end
+
       while @board.place_piece(user_response,'X') == false
         puts "column full, choose a row other than #{user_response}"
         user_response = @user_interface.getinput.upcase
       end
 
     else
-      #ai turnstuff here
       comp_response = @user_interface.compinput.upcase
-      # @board.place_piece(comp_respone, 'O')
+      # get random column choice for computers turn
       while @board.place_piece(comp_response,'O') == false
         comp_response = @user_interface.compinput.upcase
       end
